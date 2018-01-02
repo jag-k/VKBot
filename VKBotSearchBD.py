9b5ca955aba0305cf89eb94ead16dc255333329a
Jag_K <jag-k58@ya.ru>
2018-01-02 11:33:49 +0300

    Исправленны костыли в repl

--- VKBotSearchBD.py
+++ VKBotSearchBD.py
@@ -14,7 +14,7 @@ def search_bd(user_id, shift=10):
         return res + rec_search_get(age_from, age_to, offset=offset+1, **keys)
 
     def repl(s: str):
-        return ' '.join('0' if all(j == '0' for j in i) else i.lstrip('0') for i in s.split())
+        return ' '.join(int(i) if i.isdigit() else i for i in s.split())
     
     def date_search(age):
         month = {0: 0, 1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
